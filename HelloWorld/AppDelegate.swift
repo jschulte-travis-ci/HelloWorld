703c78a1486dee9eec0d79640efab87bb8570ceb
Nils Fischer <n.fischer@viwid.com>
2015-03-06 21:56:24 +0100

    initial commit

---
+++ HelloWorld/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  HelloWorld
+//
+//  Created by Nils Fischer on 06.03.15.
+//  Copyright (c) 2015 Nils Fischer. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        // Dieser Code wird beim Start der App ausgeführt
+        
+        return true
+    }
+
+}
+
---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Nils Fischer on 06.03.15.
+//  Copyright (c) 2015 Nils Fischer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
