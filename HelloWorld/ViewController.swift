7b46bd021bb6d9e2cf56de55c5a84ac61a552cd8
Nils Fischer <n.fischer@viwid.com>
2016-04-15 20:02:47 +0200

    implemented UI

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,5 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-}
+    @IBOutlet var nameTextfield: UITextField!
+    @IBOutlet var greetingLabel: UILabel!
 
+    @IBAction func greetingButtonPressed(_ sender: UIButton) {
+        if let name = nameTextfield.text, !name.isEmpty {
+            greetingLabel.text = "Hello \(name)! 👋"
+        } else {
+            greetingLabel.text = "Hello World!"
+        }
+    }
+    
+}
